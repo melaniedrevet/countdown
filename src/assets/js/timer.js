9c8de96e291924d3dbb0e084c701fd07b934089d
melaniedrevet <melanie.univ@gmail.com>
2022-03-16 11:25:26 +0100

    16/03 - Initial commit

---
+++ src/assets/js/timer.js
@@ -0,0 +1,48 @@
+window.onload = function(){
+
+    var minutes = 00;
+    var secondes = 00;
+
+    var getSecondes = document.getElementById("secondes");
+    var getMinutes = document.getElementById("minutes");
+    var buttonStart = this.document.getElementById("start");
+    var buttonStop = this.document.getElementById("stop");
+    var buttonReset = this.document.getElementById("reset");
+
+    var Interval;
+
+    function startTimer(){
+        secondes++;
+        if(secondes <= 9){
+            getSecondes.innerHTML = "0" + secondes;
+        }
+        if(secondes > 9){
+            getSecondes.innerHTML = secondes;
+        }
+        if(secondes > 59){
+            minutes++;  
+            getMinutes.innerHTML = "0" + minutes;
+            secondes = 0;
+            getSecondes.innerHTML = "0" + secondes;
+        }
+        if(minutes > 9){
+            getMinutes.innerHTML = minutes;
+        }
+    }
+    buttonStart.onclick = function(){
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 1000);
+    }
+
+    buttonStop.onclick = function(){
+        clearInterval(Interval);
+    }
+
+    buttonReset.onclick = function(){
+        clearInterval(Interval);
+        secondes = "00";
+        minutes = "00";
+        getSecondes.innerHTML = secondes;
+        getMinutes.innerHTML = minutes;
+    }     
+}
